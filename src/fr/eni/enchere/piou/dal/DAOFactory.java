de19f397279e274eff2c47f45b97429dbc18d5ad
Aheuf <clement.genty@gmail.com>
2021-07-15 14:36:14 +0200

    Modif typo DAOFactory.java

--- src/fr/eni/enchere/piou/dal/DAOFactory.java
+++ src/fr/eni/enchere/piou/dal/DAOFactory.java
@@ -1,7 +1,7 @@
 package fr.eni.enchere.piou.dal;
 
 import fr.eni.enchere.piou.bo.ArticleVendu;
-import fr.eni.enchere.piou.bo.Categories;
+import fr.eni.enchere.piou.bo.Categorie;
 import fr.eni.enchere.piou.bo.Enchere;
 import fr.eni.enchere.piou.bo.Retrait;
 import fr.eni.enchere.piou.bo.Utilisateur;
@@ -11,11 +11,11 @@ public abstract class DAOFactory {
 	public static DAO<Utilisateur>getUtilisateurDAO(){
 		return new UtilisateurDAOJdbcImpl();}
 	
-	public static DAO<ArticleVendu>getUtilisateurDAO(){
+	public static DAO<ArticleVendu>getArticleVenduDAO(){
 		return new ArticleVenduDAOJdbcImpl();}
 	
-	public static DAO<Categories>getCategoriesDAO(){
-		return new CategoriesDAOJdbcImpl();}
+	public static DAO<Categorie>getCategoriesDAO(){
+		return new CategorieDAOJdbcImpl();}
 	
 	public static DAO<Enchere>getEnchereDAO(){
 		return new EnchereDAOJdbcImpl();}
