607bf7d327bd90dafcebae423db12b014967ffa5
Aheuf <clement.genty@gmail.com>
2021-07-19 23:04:54 +0200

    modif jsp detailEncheres + fin ServletDetails

--- src/fr/eni/enchere/piou/servlets/ServletDetails.java
+++ src/fr/eni/enchere/piou/servlets/ServletDetails.java
@@ -28,57 +28,58 @@ public class ServletDetails extends HttpServlet {
 		EnchereManager em = new EnchereManager();
 		ArticleVendu article = null;
 		Categorie categorie = null;
-		Utilisateur utilisateur = null;
+		Utilisateur vendeur = null;
 		Retrait retrait = null;
-		
+// gestion de l'affichage de l'article		
 		try {
 			List<ArticleVendu> articles = em.selectArticleVenduById(idArticle);
 			article = articles.get(0);
-			
 			List <Categorie> categories = em.selectCategorieById(article.getNoCategorie());
 			categorie = categories.get(0);
-			
 			List<Utilisateur> utilisateurs = em.selectUtilisateurById(article.getNoUtilisateur());
-			utilisateur = utilisateurs.get(0);
-			
+			vendeur = utilisateurs.get(0);
 			List<Retrait> retraits = em.selectRetraitById(idArticle);
 			retrait = retraits.get(0);
 			
 		} catch (BusinessException e) {
-			System.out.println("GET ServletDetails déconne");
+			System.out.println("GET ServletDetails déconne sur l'article");
+			e.printStackTrace();
+		}
+		request.setAttribute("article", article);
+		request.setAttribute("Nom", article.getNomArticle());
+		request.setAttribute("Description", article.getDescription());
+		request.setAttribute("Categorie", categorie.getLibelle());
+		request.setAttribute("MeilleureOffre", article.getPrixVente());
+		request.setAttribute("MiseAPrix", article.getPrixInitial());
+		request.setAttribute("Retrait", retrait.toString()); // adresse de retrait
+		request.setAttribute("Vendeur", vendeur.getNom());
+// fin de gestion de l'affichage de l'article
+
+// gestion de l'affichage suivant l'utilisateur (vendeur ou non)
+		Cookie[] cookies = request.getCookies();
+		int idUtilisateur = 0;
+		
+		for (Cookie c : cookies) {
+			if(c.getName().equals("idUtilisateur")) {
+				idUtilisateur = Integer.parseInt(c.getValue());
+				break;
+			}
+		}
+		request.setAttribute("idUtilisateur", idUtilisateur);
+		request.setAttribute("idVendeur", vendeur.getNoUtilisateur());
+		
+		String vainqueur = null;
+		try {
+			List<Utilisateur> vainqueurs = em.selectUtilisateurById(article.getNoUtilisateur());
+			vainqueur = vainqueurs.get(0).getPseudo();
+		} catch (BusinessException e) {
+			System.out.println("GET ServletDetails déconne sur l'user");
 			e.printStackTrace();
 		}
 		
-// définition du cookies transportant l'id de l'article
-			Cookie[] cookies = request.getCookies();
-			Cookie cookieIdArticle = null;
-			if (cookies.length == 0) {
-				cookieIdArticle = new Cookie("idArticle",String.valueOf(idArticle));
-				cookieIdArticle.setMaxAge(Integer.MAX_VALUE);	
-			} else {
-				int i = 0;
-				for (Cookie c : cookies) {
-					i++;
-					if (c.getName().equals("idArticle")) {
-						c.setValue(String.valueOf(idArticle));
-						break;
-					} else if (i == cookies.length-1) {
-						cookieIdArticle = new Cookie("idArticle",String.valueOf(idArticle));
-						cookieIdArticle.setMaxAge(Integer.MAX_VALUE);	
-					}
-				}
-			}
-			response.addCookie(cookieIdArticle);
-// fin de la gestion du cookie IdArticle
-			
-			request.setAttribute("Nom", article.getNomArticle());
-			request.setAttribute("Description", article.getDescription());
-			request.setAttribute("Categorie", categorie.getLibelle());
-			request.setAttribute("MeilleureOffre", article.getPrixVente());
-			request.setAttribute("MiseAPrix", article.getPrixInitial());
-			request.setAttribute("Retrait", retrait.toString()); // adresse de retrait
-			request.setAttribute("Vendeur", utilisateur.getNom());
-
+		request.setAttribute("vainqueur", vainqueur);
+//fin de gestion de l'affichage suivant l'utilisateur
+		
 		rd = request.getRequestDispatcher("/WEB-INF/AffichageDetailsEncheres.jsp");
 		rd.forward(request, response);
 	}
