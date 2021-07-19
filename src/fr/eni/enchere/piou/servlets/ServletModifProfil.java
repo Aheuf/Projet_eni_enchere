84395832d731e4794e1de2e7b991f2cdc1baa463
ThePankar <87303964+ThePankar@users.noreply.github.com>
2021-07-19 23:25:02 +0200

    maj/

--- src/fr/eni/enchere/piou/servlets/ServletModifProfil.java
+++ src/fr/eni/enchere/piou/servlets/ServletModifProfil.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ServletModifProfil
  */
-@WebServlet("/encheres/ModifierProfi")
+@WebServlet("/encheres/ModifierProfil")
 public class ServletModifProfil extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
